8a8d0f523713a438820816a2c65877e2522a51fd
Joshua <joshuarauch@hotmail.com>
2024-07-01 10:48:23 +0200

    creating project components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PortafolioComponent } from './portafolio/portafolio.component';
+import { ReferencesComponent } from './references/references.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { ExtraComponent } from './extra/extra.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    PortafolioComponent,
+    ReferencesComponent,
+    ContactComponent,
+    FooterComponent,
+    ExtraComponent
   ],
   imports: [
     BrowserModule,
