57ef19505e8607c00200e8a17a207a9479e340f0
Stumpjumper1804 <stumpjumper@gmx.at>
2024-07-04 11:29:30 +0200

    added sweet alert to details windows

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -3,27 +3,31 @@ import { ActivatedRoute } from '@angular/router';
 import { CartService } from '../service/cart.service';
 import { products } from './../product';
 import { IProducts } from './../iproducts';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrl: './details.component.css'
+  styleUrl: './details.component.css',
 })
 export class DetailsComponent {
-  items:IProducts[];
+  items: IProducts[];
   item: IProducts;
 
-  constructor(
-    private route: ActivatedRoute,
-    private cartService: CartService
-   ){
-    this.items = products
-    const id = +this.route.snapshot.params["x"];
+  constructor(private route: ActivatedRoute, private cartService: CartService) {
+    this.items = products;
+    const id = +this.route.snapshot.params['x'];
     this.item = this.items[id];
   }
 
-  addToCart(){
-     this.cartService.addToCart(this.item)
+  addToCart() {
+    this.cartService.addToCart(this.item);
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Added to cart',
+      showConfirmButton: false,
+      timer: 1200,
+    });
   }
 }
-
